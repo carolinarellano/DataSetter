1bd862a4bb1d6428176e3ab738fd11e5b196fd78
Rudrok <100793160+Rudrok@users.noreply.github.com>
2023-05-14 16:03:42 -0600

    ola

--- src/main/java/rcy/Main.java
+++ src/main/java/rcy/Main.java
@@ -1,15 +1,25 @@
 package rcy;
 
 import rcy.minecraft.Armor;
+import rcy.minecraft.ArmorMaterials;
+
+import rcy.minecraft.Weapon;
+import rcy.minecraft.WeaponMaterials;
+import static rcy.minecraft.Tool.*;
 
 import java.io.IOException;
 import java.text.ParseException;
 
-import static rcy.minecraft.ArmorMaterials.*;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException {
-        //Armor.registerJava(LEATHER);
-        Armor.registerJson(LEATHER);
+        //Armor.registerJava(ArmorMaterials.LEATHER);
+        //Armor.registerJson(ArmorMaterials.DIAMOND);
+        Weapon.registerJava(WeaponMaterials.CITRINE, SWORD, 6, 3.2f);
+        Weapon.registerJava(WeaponMaterials.IRON, PICKAXE, 3, 2f);
+        Weapon.registerJava(WeaponMaterials.DIAMOND, AXE, 8, 7f);
+        Weapon.registerJava(WeaponMaterials.STONE, SHOVEL, 2, 1.3f);
+        Weapon.registerJava(WeaponMaterials.NETHERITE, HOE, 1, 0.3f);
+
     }
 }
